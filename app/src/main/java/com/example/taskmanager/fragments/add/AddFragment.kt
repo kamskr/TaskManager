88cacef994b449248b89fc352aa8fd52c68b675b
Kamil <sikorakamil26@gmail.com>
2022-04-24 00:49:49 +0200

    added date picker

--- app/src/main/java/com/example/taskmanager/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/taskmanager/fragments/add/AddFragment.kt
@@ -5,14 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.example.taskmanager.R
+import java.text.SimpleDateFormat
+import java.util.*
+import com.example.taskmanager.MainActivity
+
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+
+import android.widget.DatePicker
+
+
+
 
 class AddFragment : Fragment() {
+    private val myCalendar: Calendar = Calendar.getInstance()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+        val editText = view.findViewById<EditText>(R.id.et_deadline)
+        val date =
+            OnDateSetListener { view, year, month, day ->
+                myCalendar[Calendar.YEAR] = year
+                myCalendar[Calendar.MONTH] = month
+                myCalendar[Calendar.DAY_OF_MONTH] = day
+                updateLabel(editText)
+            }
+
+        editText.setOnClickListener(View.OnClickListener {
+            DatePickerDialog(
+                view.context,
+                date,
+                myCalendar[Calendar.YEAR],
+                myCalendar[Calendar.MONTH],
+                myCalendar[Calendar.DAY_OF_MONTH]
+            ).show()
+        })
+        return view
+    }
+
+    private fun updateLabel(editText: EditText) {
+        val myFormat = "MM/dd/yy"
+        val dateFormat = SimpleDateFormat(myFormat, Locale.US)
+        editText.setText(dateFormat.format(myCalendar.time))
     }
 }
